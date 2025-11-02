1cc5541fb86c0cfebc2d1d15d545d29eae933ee8
emelikrebs <119435249+emelikrebs@users.noreply.github.com>
2025-11-02 11:16:38 +0100

    Add files via upload

---
+++ build/generated/src/org/apache/jsp/eventos_jsp.java
@@ -0,0 +1,105 @@
+package org.apache.jsp;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.jsp.*;
+
+public final class eventos_jsp extends org.apache.jasper.runtime.HttpJspBase
+    implements org.apache.jasper.runtime.JspSourceDependent {
+
+  private static final JspFactory _jspxFactory = JspFactory.getDefaultFactory();
+
+  private static java.util.List<String> _jspx_dependants;
+
+  private org.glassfish.jsp.api.ResourceInjector _jspx_resourceInjector;
+
+  public java.util.List<String> getDependants() {
+    return _jspx_dependants;
+  }
+
+  public void _jspService(HttpServletRequest request, HttpServletResponse response)
+        throws java.io.IOException, ServletException {
+
+    PageContext pageContext = null;
+    HttpSession session = null;
+    ServletContext application = null;
+    ServletConfig config = null;
+    JspWriter out = null;
+    Object page = this;
+    JspWriter _jspx_out = null;
+    PageContext _jspx_page_context = null;
+
+    try {
+      response.setContentType("text/html;charset=UTF-8");
+      pageContext = _jspxFactory.getPageContext(this, request, response,
+      			null, true, 8192, true);
+      _jspx_page_context = pageContext;
+      application = pageContext.getServletContext();
+      config = pageContext.getServletConfig();
+      session = pageContext.getSession();
+      out = pageContext.getOut();
+      _jspx_out = out;
+      _jspx_resourceInjector = (org.glassfish.jsp.api.ResourceInjector) application.getAttribute("com.sun.appserv.jsp.resource.injector");
+
+      out.write("\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("<!DOCTYPE html>\n");
+      out.write("<html>\n");
+      out.write("    <head>\n");
+      out.write("        <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n");
+      out.write("        <title>JSP Page</title>\n");
+      out.write("        <link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css\">\n");
+      out.write("        <link href=\"https://fonts.googleapis.com/icon?family=Material+Icons\" rel=\"stylesheet\">\n");
+      out.write("        <script src=\"https://code.jquery.com/jquery-3.3.1.min.js\" integrity=\"sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=\"  crossorigin=\"anonymous\"></script>\n");
+      out.write("    </head>\n");
+      out.write("    <body>\n");
+      out.write("        <header>\n");
+      out.write("            ");
+      org.apache.jasper.runtime.JspRuntimeLibrary.include(request, response, "cabecalho.jsp", out, true);
+      out.write("\n");
+      out.write("        </header>\n");
+      out.write("\n");
+      out.write("        <main>\n");
+      out.write("            <body>\n");
+      out.write("                <form action=\"index.jsp\" method=\"post\" >\n");
+      out.write("                    <div class=\"row\">\n");
+      out.write("                        <div class=\"col s12 m6\">\n");
+      out.write("                            <div class=\"card\">\n");
+      out.write("                                <div class=\"card-image\">\n");
+      out.write("                                    <img src=\"imagens/concurso-literario.png\">\n");
+      out.write("                                    <span class=\"card-title\">Concurso Literário</span>\n");
+      out.write("                                    <a class=\"btn-floating halfway-fab waves-effect waves-light red\"><i class=\"material-icons\">add</i></a>\n");
+      out.write("                                </div>\n");
+      out.write("                                <div class=\"card-content\">\n");
+      out.write("                                    <p></p>\n");
+      out.write("                                </div>\n");
+      out.write("                            </div>\n");
+      out.write("                        </div>\n");
+      out.write("                    </div>\n");
+      out.write("                </form>\n");
+      out.write("            </body>\n");
+      out.write("        </main>\n");
+      out.write("\n");
+      out.write("        <footer>\n");
+      out.write("            ");
+      org.apache.jasper.runtime.JspRuntimeLibrary.include(request, response, "rodape.jsp", out, true);
+      out.write("\n");
+      out.write("        </footer>                \n");
+      out.write("        <script src=\"https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js\"></script>\n");
+      out.write("\n");
+      out.write("    </body>\n");
+      out.write("</html>\n");
+    } catch (Throwable t) {
+      if (!(t instanceof SkipPageException)){
+        out = _jspx_out;
+        if (out != null && out.getBufferSize() != 0)
+          out.clearBuffer();
+        if (_jspx_page_context != null) _jspx_page_context.handlePageException(t);
+        else throw new ServletException(t);
+      }
+    } finally {
+      _jspxFactory.releasePageContext(_jspx_page_context);
+    }
+  }
+}
---
+++ build/generated/src/org/apache/jsp/login_jsp.java
@@ -0,0 +1,132 @@
+package org.apache.jsp;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.jsp.*;
+
+public final class login_jsp extends org.apache.jasper.runtime.HttpJspBase
+    implements org.apache.jasper.runtime.JspSourceDependent {
+
+  private static final JspFactory _jspxFactory = JspFactory.getDefaultFactory();
+
+  private static java.util.List<String> _jspx_dependants;
+
+  private org.glassfish.jsp.api.ResourceInjector _jspx_resourceInjector;
+
+  public java.util.List<String> getDependants() {
+    return _jspx_dependants;
+  }
+
+  public void _jspService(HttpServletRequest request, HttpServletResponse response)
+        throws java.io.IOException, ServletException {
+
+    PageContext pageContext = null;
+    HttpSession session = null;
+    ServletContext application = null;
+    ServletConfig config = null;
+    JspWriter out = null;
+    Object page = this;
+    JspWriter _jspx_out = null;
+    PageContext _jspx_page_context = null;
+
+    try {
+      response.setContentType("text/html;charset=UTF-8");
+      pageContext = _jspxFactory.getPageContext(this, request, response,
+      			null, true, 8192, true);
+      _jspx_page_context = pageContext;
+      application = pageContext.getServletContext();
+      config = pageContext.getServletConfig();
+      session = pageContext.getSession();
+      out = pageContext.getOut();
+      _jspx_out = out;
+      _jspx_resourceInjector = (org.glassfish.jsp.api.ResourceInjector) application.getAttribute("com.sun.appserv.jsp.resource.injector");
+
+      out.write("\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("<!DOCTYPE html>\n");
+      out.write("<html>\n");
+      out.write("    <head>\n");
+      out.write("        <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n");
+      out.write("        <title>Eventos IFPR</title>\n");
+      out.write("        <link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css\">\n");
+      out.write("        <link href=\"https://fonts.googleapis.com/icon?family=Material+Icons\" rel=\"stylesheet\">\n");
+      out.write("        <script src=\"https://code.jquery.com/jquery-3.3.1.min.js\" integrity=\"sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=\"  crossorigin=\"anonymous\"></script>\n");
+      out.write("\n");
+      out.write("        ");
+
+            String e = request.getParameter("e");
+            String mensagem = "";
+            if (e != null) {
+                mensagem = e;
+            }
+        
+      out.write("\n");
+      out.write("\n");
+      out.write("    </head>\n");
+      out.write("    <body>\n");
+      out.write("        <header>\n");
+      out.write("            ");
+      org.apache.jasper.runtime.JspRuntimeLibrary.include(request, response, "cabecalho.jsp", out, true);
+      out.write("\n");
+      out.write("        </header>\n");
+      out.write("\n");
+      out.write("        <main>\n");
+      out.write("\n");
+      out.write("            <p>");
+      out.print(mensagem);
+      out.write("</p>\n");
+      out.write("\n");
+      out.write("            <div class=\"row\">\n");
+      out.write("                <form action=\"scripts/login.jsp\" method=\"POST\" class=\"col s12\">\n");
+      out.write("                    <div class=\"row\">\n");
+      out.write("                        <br>\n");
+      out.write("                        <div class=\"input-field col s12\">\n");
+      out.write("                            <i class=\"material-icons prefix\">person</i>\n");
+      out.write("                            <input id=\"usuario\" name=\"usuario\" type=\"text\" class=\"validate\">\n");
+      out.write("                            <label for=\"usuario\">Usuario</label>\n");
+      out.write("                        </div>\n");
+      out.write("                    </div>\n");
+      out.write("\n");
+      out.write("                    <div class=\"row\">\n");
+      out.write("                        <div class=\"input-field col s12\">\n");
+      out.write("                            <i class=\"material-icons prefix\">https</i>\n");
+      out.write("                            <input id=\"senha\" name=\"senha\" type=\"password\" class=\"validate\">\n");
+      out.write("                            <label for=\"senha\">Senha</label>\n");
+      out.write("                        </div>\n");
+      out.write("                    </div>\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("                    <div class=\"row\">\n");
+      out.write("                        <div class=\"input-field col s12\">\n");
+      out.write("                            <button type=\"submit\" class=\"waves-effect waves-light btn\"><i class=\"material-icons right\">send</i>Entrar</button>\n");
+      out.write("                        </div>\n");
+      out.write("                    </div>\n");
+      out.write("\n");
+      out.write("                </form>\n");
+      out.write("            </div>\n");
+      out.write("        </main>\n");
+      out.write("\n");
+      out.write("        <footer>\n");
+      out.write("            ");
+      org.apache.jasper.runtime.JspRuntimeLibrary.include(request, response, "rodape.jsp", out, true);
+      out.write("\n");
+      out.write("        </footer>                \n");
+      out.write("        <script src=\"https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js\"></script>        \n");
+      out.write("    </body>\n");
+      out.write("</html>");
+    } catch (Throwable t) {
+      if (!(t instanceof SkipPageException)){
+        out = _jspx_out;
+        if (out != null && out.getBufferSize() != 0)
+          out.clearBuffer();
+        if (_jspx_page_context != null) _jspx_page_context.handlePageException(t);
+        else throw new ServletException(t);
+      }
+    } finally {
+      _jspxFactory.releasePageContext(_jspx_page_context);
+    }
+  }
+}
